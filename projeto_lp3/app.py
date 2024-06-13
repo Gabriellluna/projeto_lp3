360d71e49bf8f0bb17f8b48f1d94ced2e185de40
Gabriellluna <glunamaia@gmail.com>
2024-06-13 16:52:29 -0300

    Primeiras funções nas rotas

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from validate_docbr import CPF
+from validate_docbr import CNPJ
 
+cnpj = CNPJ()
+cpf = CPF()
 app = Flask("Minha App")
 
 # Uma página do flask é igual a: rota + função
@@ -8,7 +12,7 @@ app = Flask("Minha App")
 @app.route("/")
 
 def home():
-    return "<h1> Home Page </h1>"
+    return "<h1> Home Page!!! </h1>"
 
 #/ contato - página de contato
 @app.route("/contato")
@@ -17,7 +21,44 @@ def contato():
     return "<h1> Contato </h1>"
 
 #/produtos - pagina produtos
-@app.route("/produtos")
+@app.route("/produto")
 
 def produto():
-    return "<h1> Produtos Legais</h1>"
+    return "<h1> Produtos Legais </h1>"
+
+#/produtos - pagina servicos
+@app.route("/servicos")
+
+def servicos():
+    return "<h1> Nossos serviços são: Emissão de notas fiscais, abertura de empresa e fechamento de empresa  </h1>"
+
+#/produtos - pagina gerar-cpf
+@app.route("/gerar-cpf")
+
+def gerarcpf():
+    cpfs = []
+    for i in range(10):
+        cpfs.append(cpf.generate(True))
+        
+    return  f'Aki: {cpfs} '
+#/produtos - pagina gerar-cnpj
+@app.route("/gerar-cnpj")
+
+def gerarcnpj():
+    cnpjs = []
+
+    for i in range(10):
+        cnpjs.append(cpf.generate(True))
+        
+    return  f'Aki: {cnpjs} '
+    
+
+#pagina/ servicos retornar "Nosso serviços"
+#pagina/ gerar-cpf retornar cpf aleatorio
+#pagina/ gerar-cnpj retornar cnpj aleatorio
+
+
+
+
+
+app.run(debug=True)
